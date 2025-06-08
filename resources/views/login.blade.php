386647c526a7d1de3bdaa6593d40fc27a33f79a5
ananda1009 <ananda.paul55@gmail.com>
2025-06-09 01:46:42 +0530

    part1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,15 +3,16 @@
 <div class="container custome-style">
   <div class="row">
     <div class="col-sm-4 col-sm-offset-4">
-      <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
+   <form method="POST" action="/login">
+        @csrf
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
  
  
   <button type="submit" class="btn btn-primary">Submit</button>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::view('/login','login');
+Route::post('/login',[UserController::class,'login']);
+Route::post('/',[ProductController::class,'index']);
